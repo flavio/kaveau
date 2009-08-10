93a7799c59f3ee6e2541471c2edc7b223620747e
Flavio Castelli <fcastelli@novell.com>
2009-08-10 12:16:19 +0200

    fixed copyright (switched from LGPL to GPL)

--- src/utils/rdiffmanager.cpp
+++ src/utils/rdiffmanager.cpp
@@ -1,22 +1,22 @@
-/* This file is part of kaveau
+/* This file is part of the kaveau project
  * The base of this file has been taken from the Keep project
  *
  * Copyright (C) 2009 Flavio Castelli <flavio@castelli.name>
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * kaveau is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * kaveau is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Library General Public License
- * along with kaveau; see the file COPYING.LIB.  If not, write to
- * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301, USA.
+ * You should have received a copy of the GNU General Public License
+ * along with Keep; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Steet, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
 #include "rdiffmanager.h"
